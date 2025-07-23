6ba6c9d9b549552910ceab52bbd2b1ad10d5292e
Sven Seeberg <mail@sven-seeberg.de>
2025-07-23 15:35:45 +0200

    Filter branches and validate request

--- sbom_archiver/wsgi.py
+++ sbom_archiver/wsgi.py
@@ -5,6 +5,9 @@ import configparser
 import json
 import os
 
+from hashlib import sha256
+from hmac import HMAC, compare_digest
+
 import requests
 from flask import Flask, request, jsonify
 
@@ -25,24 +28,30 @@ try:
 except:
     sbom_repo = Repo.clone_from(SBOM_ARCHIVE_REPO_URL, SBOM_ARCHIVE_REPO_LOCAL_PATH)
 
+def verify_signature(req, token):
+     received_sign = req.headers.get('X-Hub-Signature-256').split('sha256=')[-1].strip()
+     secret = token.encode()
+     expected_sign = HMAC(key=secret, msg=req.data, digestmod=sha256).hexdigest()
+     return compare_digest(received_sign, expected_sign)
+
 @application.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
-    if 'repo_name' not in data or 'commit_hash' not in data:
+    if 'ref' not in data or 'repository' not in data:
         return jsonify({'error': 'Missing required arguments'}), 400
 
-    repo_name = data['repo_name']
-    commit_hash = data['commit_hash']
-    owner, repo = repo_name.split('/')
+    repo = data['repository']['name']
+    owner = data['repository']['owner']['name']
+    commit_hash = data['after']
+    org_repo_name = f"{owner}/{repo}"
 
-    auth_header = request.headers.get('Authorization')
-    if not auth_header or not auth_header.startswith('Bearer '):
-        return jsonify({'error': 'Missing or invalid Authorization header'}), 401
-    token = auth_header.split(' ')[1]
-    if token != VALID_TOKENS[repo_name]:
-        return jsonify({'error': 'Invalid token'}), 401
+    if not verify_signature(request, VALID_TOKENS[org_repo_name]):
+        return jsonify({'error': 'Invalid or missing token'}), 401
 
-    url = f"https://api.github.com/repos/{owner}/{repo}/dependency-graph/sbom"
+    if not data["ref"] in ["refs/heads/develop", "refs/heads/master", "refs/heads/main"]:
+        return jsonify({'message': 'Not main, master or develop branch. Not archiving SBOM.'}), 200
+
+    url = f"https://api.github.com/repos/{org_repo_name}/dependency-graph/sbom"
     headers = {
         'Authorization': f'token {GITHUB_TOKEN}',
         'Accept': 'application/json'
@@ -53,17 +62,17 @@ def webhook():
         return jsonify({'error': 'Failed to fetch SBOM'}), 500
     sbom_data = response.json()
 
-    repo_path = f"{SBOM_ARCHIVE_REPO_LOCAL_PATH}/{repo_name}"
+    repo_path = f"{SBOM_ARCHIVE_REPO_LOCAL_PATH}/{org_repo_name}"
     if not os.path.exists(repo_path):
         os.makedirs(repo_path)
-    filename = f"{repo_name}/{commit_hash}.json"
+    filename = f"{org_repo_name}/{commit_hash}.json"
     filepath = f"{SBOM_ARCHIVE_REPO_LOCAL_PATH}/{filename}"
 
     with open(filepath, 'w') as f:
         f.write(json.dumps(sbom_data, indent=2))
 
     sbom_repo.index.add([filepath])
-    sbom_repo.index.commit(f"Add {repo_name} SBOM for {commit_hash}")
+    sbom_repo.index.commit(f"Add {org_repo_name} SBOM for {commit_hash}")
     origin = sbom_repo.remotes.origin
     origin.push()
 
@@ -71,3 +80,5 @@ def webhook():
 
 if __name__ == '__main__':
     application.run()
+
+
